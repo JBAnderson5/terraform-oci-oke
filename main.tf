bbde19fd1fdbc069b7fdc5604ee58d46df834d0f
JB <35785660+JBAnderson5@users.noreply.github.com>
2021-08-11 09:47:19 -0700

    updated modules to skip base module and use custom vcn module

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 # Copyright 2017, 2019, Oracle Corporation and/or affiliates.  All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl
 
+/*
 module "base" {
-  source  = "oracle-terraform-modules/base/oci"
-  version = "2.4.0"
+  source  = "./modules/base"
+  #version = "2.4.0"
 
   # general oci parameters
   oci_base_general = local.oci_base_general
@@ -20,6 +21,126 @@ module "base" {
   # operator server parameters
   oci_base_operator = local.oci_base_operator
 
+}
+
+*/
+
+module "vcn" {
+  source  = "../terraform-oci-vcn"
+  #version = "2.3.0"
+
+  # provider parameters
+  region = var.region
+
+  # general oci parameters
+  compartment_id = var.compartment_id #TODO: should we change this to refer to network compartment?
+  label_prefix   = var.label_prefix
+  tags           = var.tags["vcn"]
+
+  # vcn parameters
+  create_drg               = var.create_drg
+  drg_display_name         = var.drg_display_name
+  internet_gateway_enabled = true
+  lockdown_default_seclist = var.lockdown_default_seclist
+  nat_gateway_enabled      = var.worker_mode == "private" || var.operator_enabled == true || (var.lb_subnet_type == "internal" || var.lb_subnet_type == "both") ? true : false
+  nat_gateway_public_ip_id = var.nat_gateway_public_ip_id
+  service_gateway_enabled  = true
+  vcn_cidr                 = var.vcn_cidr
+  vcn_dns_label            = var.vcn_dns_label
+  vcn_name                 = var.vcn_name
+
+  # routing rules
+  internet_gateway_route_rules = var.internet_gateway_route_rules
+  nat_gateway_route_rules      = var.nat_gateway_route_rules
+}
+
+module "bastion" {
+  source  = "oracle-terraform-modules/bastion/oci"
+  version = "2.1.0"
+
+  # provider identity parameters
+  api_fingerprint      = var.api_fingerprint
+  api_private_key_path = var.api_private_key_path
+  region               = var.region
+  tenancy_id           = var.tenancy_id
+  user_id              = var.user_id
+
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
+
+  # network parameters
+
+  availability_domain = var.availability_domains["bastion"]
+  bastion_access      = var.bastion_access
+  ig_route_id         = module.vcn.ig_route_id
+  netnum              = var.netnum["bastion"]
+  newbits             = var.newbits["bastion"]
+  vcn_id              = module.vcn.vcn_id
+
+  # bastion parameters
+  bastion_enabled                  = var.bastion_enabled
+  bastion_image_id                 = var.bastion_image_id
+  bastion_operating_system_version = var.bastion_operating_system_version
+  bastion_shape                    = var.bastion_shape
+  bastion_state                    = var.bastion_state
+  bastion_upgrade                  = var.bastion_package_upgrade
+  ssh_public_key                   = var.ssh_public_key
+  ssh_public_key_path              = var.ssh_public_key_path
+  timezone                         = var.bastion_timezone
+
+  # notification
+  notification_enabled  = var.bastion_notification_enabled
+  notification_endpoint = var.bastion_notification_endpoint
+  notification_protocol = var.bastion_notification_protocol
+  notification_topic    = var.bastion_notification_topic
+
+  # tags
+  tags = var.tags["bastion"]
+}
+
+module "operator" {
+  source  = "oracle-terraform-modules/operator/oci"
+  version = "2.2.0"
+
+  # provider identity parameters
+  api_fingerprint      = var.api_fingerprint
+  api_private_key_path = var.api_private_key_path
+  region               = var.region
+  tenancy_id           = var.tenancy_id
+  user_id              = var.user_id
+
+  # general oci parameters
+  compartment_id = var.compartment_id
+  label_prefix   = var.label_prefix
+
+  # network parameters
+  availability_domain = var.availability_domains["operator"]
+  nat_route_id        = module.vcn.nat_route_id
+  netnum              = var.netnum["operator"]
+  newbits             = var.newbits["operator"]
+  vcn_id              = module.vcn.vcn_id
+
+  # operator parameters
+  operator_enabled            = var.operator_enabled
+  operator_image_id           = var.operator_image_id
+  operator_instance_principal = var.operator_instance_principal
+  operator_shape              = var.operator_shape
+  operator_state              = var.operator_state
+  operating_system_version    = var.operator_version
+  operator_upgrade            = var.operator_package_upgrade
+  ssh_public_key              = var.ssh_public_key
+  ssh_public_key_path         = var.ssh_public_key_path
+  timezone                    = var.operator_timezone
+
+  # notification
+  notification_enabled  = var.operator_notification_enabled
+  notification_endpoint = var.operator_notification_endpoint
+  notification_protocol = var.operator_notification_protocol
+  notification_topic    = var.operator_notification_topic
+
+  # tags
+  tags = var.tags["bastion"]
 }
 
 module "policies" {
